fd6bb6c769f676df5896378efd286d348639fcf5
med420 <mohamed0cherve@gmail.com>
2025-07-16 04:09:45 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  toggleBtn.textContent = body.classList.contains("dark") ? "☀️ Mode Clair" : "🌙 Mode Sombre";
+});
